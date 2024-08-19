833fe096a242ca33ed54a0ef60eb8c0fc089702b
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-08-19 19:02:50 -0400

    fix tooltip from being clickable

--- scripts/leetcode/submitBtn.js
+++ scripts/leetcode/submitBtn.js
@@ -12,7 +12,7 @@ const createToolTip = () => {
   toolTip.textContent =
     'Manually upload this submission to GitHub (beta).\nThis will OVERWRITE your current submission.\nPlease be mindful of your GitHub rate-limits.';
   toolTip.className =
-    'fixed bg-sd-popover text-sd-popover-foreground rounded-sd-md z-modal text-xs text-left font-normal whitespace-pre-line shadow p-3 border-sd-border border cursor-default translate-y-20 transition-opacity opacity-0 transition-delay-1000 duration-300 group-hover:opacity-100';
+    'pointer-events-none fixed bg-sd-popover text-sd-popover-foreground rounded-sd-md z-modal text-xs text-left font-normal whitespace-pre-line shadow p-3 border-sd-border border cursor-default translate-y-20 transition-opacity opacity-0 transition-delay-1000 duration-300 group-hover:opacity-100';
   return toolTip;
 };
 
